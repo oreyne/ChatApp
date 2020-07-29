9dee9f84a992036b920c8c5078f0ea001cbec53d
Oreyne Lang <oreynelang14@gmail.com>
2020-07-29 11:21:19 -0400

    created a new modal for chat room

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
+import AddRoomScreen from '../screens/AddRoomScreen';
 
 const Stack = createStackNavigator();
+const ChatAppStack = createStackNavigator();
+const ModalStack = createStackNavigator();
+
+function ChatApp() {
+	return (
+		<ChatAppStack.Navigator
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: '#6646ee'
+				}
+			}}
+		>
+			<ChatAppStack.Screen name='Home' component={HomeScreen} />
+		</ChatAppStack.Navigator>
+	);
+}
 
 export default function HomeStack() {
 	return(
-		<Stack.Navigator>
-			<Stack.Screen name='Home' component={HomeScreen} />
-		</Stack.Navigator>
+		<ModalStack.Navigator mode='modal' headerMode='nonde'>
+			<ModalStack.Screen name='ChatApp' component={ChatApp} />
+			<ModalStack.Screen name='AddRoom' component={AddRoomScreen} />
+		</ModalStack.Navigator>		
 	);
 }
