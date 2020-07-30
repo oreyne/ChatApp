b403f5718d0bf4a0a129bec357bc72443e84c256
Oreyne Lang <oreynelang14@gmail.com>
2020-07-30 19:19:55 -0400

    fix a issue with insert the message

--- src/screens/RoomScreen.js
+++ src/screens/RoomScreen.js
@@ -1,49 +1,31 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../navigation/AuthProvider';
+import React, { useState, useContext, useEffect } from 'react';
 import { GiftedChat, Bubble, Send, SystemMessage } from 'react-native-gifted-chat';
-import { IconButton } from 'react-native-paper';
 import { ActivityIndicator, View, StyleSheet } from 'react-native';
+import { IconButton } from 'react-native-paper';
+import { AuthContext } from '../navigation/AuthProvider';
 import firestore from '@react-native-firebase/firestore';
 
-export default function RoomScreen({ route }){
-	const { user } = useContext(AuthContext);
-	const currentUser = user.toJSON();
-	const { thread } = route.params;
-	const [ messages, setMessages ] = useState(
-		[
-			{
-				_id: 0,
-				text: 'New room created',
-				createdAt: new Date().getTime(),
-				system: true
-			},
-			{
-				_id: 1,
-				text: 'Hello',
-				createdAt: new Date().getTime(),
-				user: {
-					_id: 2,
-					name: 'Test User'
-				}
-			}
-		]
-	);
+export default function RoomScreen({ route }) {
+  const [messages, setMessages] = useState([]);
+  const { thread } = route.params;
+  const { user } = useContext(AuthContext);
+  const currentUser = user.toJSON();
 
-	async function handleSend(messages) {
-		const text = messages[0].text;
+  async function handleSend(messages) {
+    const text = messages[0].text;
 
-		firestore()
-		.collection('THREADS')
-		.doc(thread._id)
-		.collection('MESSAGES')
-		.add({
-			text,
-			creadAt: new Date().getTime(),
-			user: {
-				_id: currentUser.uid,
-				email: currentUser.email
-			}
-		});
+    firestore()
+      .collection('THREADS')
+      .doc(thread._id)
+      .collection('MESSAGES')
+      .add({
+        text,
+        createdAt: new Date().getTime(),
+        user: {
+          _id: currentUser.uid,
+          email: currentUser.email
+        }
+    });
 
 		await firestore()
 	    .collection('THREADS')
@@ -134,7 +116,7 @@ export default function RoomScreen({ route }){
 
 	function scrollToBottomComponent() {
 		return (
-			<View style={styles.bottonComponentContainer}>
+			<View style={styles.bottomComponentContainer}>
 				<IconButton icon='chevron-double-down' size={36} color='#6646ee' />
 			</View>
 		);
@@ -169,19 +151,27 @@ export default function RoomScreen({ route }){
 }
 
 const styles = StyleSheet.create({
-	sendingContainer: {
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
 	loadingContainer: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center'
+	    flex: 1,
+	    alignItems: 'center',
+	    justifyContent: 'center'
+	},
+	sendingContainer: {
+	    justifyContent: 'center',
+	    alignItems: 'center'
+	},
+	bottomComponentContainer: {
+	    justifyContent: 'center',
+	    alignItems: 'center'
+	},
+	systemMessageWrapper: {
+	    backgroundColor: '#6646ee',
+	    borderRadius: 4,
+	    padding: 5
 	},
 	systemMessageText: {
-		fontSize: 14,
-		color: '#fff',
-		fontWeight: 'bold',
-		backgroundColor: '#443211'
+	    fontSize: 14,
+	    color: '#fff',
+	    fontWeight: 'bold'
 	}
 });
